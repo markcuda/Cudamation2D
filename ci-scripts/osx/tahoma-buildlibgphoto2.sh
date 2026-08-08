c4f14e2e9af65921050cbcaf8967a300cc2c7362
Mark Cuda <mc@markcuda.com>
2026-08-07 23:29:26 -0400

    fix: link libgphoto2 gettext on macOS [skip ci]

--- ci-scripts/osx/tahoma-buildlibgphoto2.sh
+++ ci-scripts/osx/tahoma-buildlibgphoto2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 cd thirdparty
 
 echo ">>> Cloning libgphoto2"
@@ -11,6 +13,13 @@ git checkout tahoma2d-version-2.5.34
 echo ">>> Configuring libgphoto2"
 autoreconf --install --symlink
 
+GETTEXT_PREFIX="$(brew --prefix gettext)"
+# Camera modules do not inherit libgphoto2's gettext link flags on macOS.
+export CPPFLAGS="-I${GETTEXT_PREFIX}/include ${CPPFLAGS:-}"
+export LDFLAGS="-L${GETTEXT_PREFIX}/lib ${LDFLAGS:-}"
+export LIBS="-lintl ${LIBS:-}"
+export PKG_CONFIG_PATH="${GETTEXT_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
+
 ./configure --prefix=/usr/local
 
 echo ">>> Making libgphoto2"
@@ -18,5 +27,7 @@ make
 
 echo ">>> Installing libgphoto2"
 sudo make install
+
+test -f /usr/local/include/gphoto2/gphoto2.h
 
 cd ..
